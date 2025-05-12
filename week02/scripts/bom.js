d81759b285ab6dac77c92342b5ad587d189f5fb6
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-05-12 14:20:06 -0600

    Bom 2

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -2,10 +2,19 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        const deleteButton = document.createElement('button')
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+        });
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.append(li);
+        input.value = ''
+    }    
+})    
 
-const li = document.createElement('li');
-li.textContent = input.value;
-const deleteButton = document.createElement('button')
-deleteButton.textContent = "❌";
-li.append(deleteButton);
-list.append(li);
