54175d742899366761ba7879053e820963574d6a
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-05-23 08:58:16 -0600

    windchill

--- scripts/place.js
+++ scripts/place.js
@@ -8,11 +8,11 @@ function calculateWindChill(temperature, windSpeed) {
     if (temp <= 10 && wind >= 4.8) {
         const chill = 13.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0.16);
         windChill.textContent = `${chill.toFixed(1)} °C`;
+        
     } else {
         windChill.textContent = 'N/A';
     }
-
-    
+    console.log(windChill.textContent);
 }
 
 calculateWindChill(temperature, windSpeed)
